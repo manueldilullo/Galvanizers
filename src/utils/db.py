9023df7b7d59b6d249bafdec5297ee0b98f897e8
manueldilullo <manuel.dilullo99@gmail.com>
2023-05-20 16:57:31 +0200

    feat(utils): add utils classes for sqlite and smtp

---
+++ src/utils/db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+def DB_util():
+    def __init__(self):
+        pass
+    
+    def signup(self):        
+        # conn = sqlite3.connect('user.db')
+        # c = conn.cursor()
+        # c.execute('''CREATE TABLE IF NOT EXISTS users
+        #              (name text, surname text, email text, password text, age integer, sex text, feeling text)''')
+        # c.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)", (name, surname, email, password, age, sex, feeling))
+        # conn.commit()
+        # conn.close()
+        print("Not implemented yet")
+        pass
+
+    def login(self):
+        # conn = sqlite3.connect('user.db')
+        # c = conn.cursor()
+        # c.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
+        # if len(c.fetchall()) > 0:
+        #     conn.close()
+        #     self.controller.switch_frame(ChooseTopicPage)
+        # else:
+        #     conn.close()
+        #     self.email_entry.delete(0, tk.END)
+        #     self.password_entry.delete(0, tk.END)
+        #     self.email_entry.config(bg="red")
+        #     self.password_entry.config(bg="red")
+        print("Not implemented yet")
+        pass
+
+    def get_email(self):
+        print("Not implemented yet")
+        pass
